36f19c49dc2c3d7dbd3deb30f43b279f7efebcc7
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 <timos360@gmail.com>
2023-09-16 16:35:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [subject, setSubject] = useState('');
+  const [classNumber, setClassNumber] = useState('');
+  const [tableData, setTableData] = useState([]);
+
+  const handleSubjectChange = (event) => {
+    setSubject(event.target.value);
+  };
+
+  const handleClassNumberChange = (event) => {
+    setClassNumber(event.target.value);
+  };
+
+  const handleAddRow = () => {
+    if (subject && classNumber) {
+      const newRow = {
+        subject,
+        classNumber,
+      };
+
+      setTableData([...tableData, newRow]);
+      setSubject('');
+      setClassNumber('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Class Schedule</h1>
+      <div>
+        <label htmlFor="subject">Subject:</label>
+        <select id="subject" value={subject} onChange={handleSubjectChange}>
+          <option value="">Select a Subject</option>
+          <option value="Math">Math</option>
+          <option value="Science">Science</option>
+          <option value="History">History</option>
+          <option value="English">English</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="classNumber">Class Number:</label>
+        <input
+          type="text"
+          id="classNumber"
+          value={classNumber}
+          onChange={handleClassNumberChange}
+        />
+      </div>
+      <div>
+        <button onClick={handleAddRow}>Add Row</button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Subject</th>
+            <th>Class Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((row, index) => (
+            <tr key={index}>
+              <td>{row.subject}</td>
+              <td>{row.classNumber}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
